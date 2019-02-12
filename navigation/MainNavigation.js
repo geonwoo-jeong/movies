784458a1472efc5dde2b24812cf24ea40d07b356
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-02-12 21:58:17 +0900

    Finishing Up

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -3,9 +3,15 @@ import TabNavigation from "./TabNavigation";
 import DetailScreen from "../screens/Detail";
 import { headerStyles } from "./config";
 
-const MainNavigation = createStackNavigator({
-  Tabs: { screen: TabNavigation, navigationOptions: { header: null } },
-  Detail: { screen: DetailScreen, navigationOptions: { ...headerStyles } }
-});
+const MainNavigation = createStackNavigator(
+  {
+    Tabs: { screen: TabNavigation, navigationOptions: { header: null } },
+    Detail: { screen: DetailScreen, navigationOptions: { ...headerStyles } }
+  },
+  {
+    headerMode: "screen",
+    headerBackTitleVisible: false
+  }
+);
 
 export default createAppContainer(MainNavigation);
