e2024bd7c3d2767573059fff4107fc9ca392382b
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-02-14 20:51:43 +0900

    Making a Slider part One

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,5 +1,6 @@
-export const BG_COLOR = "black";
+export const BLACK = "black";
 export const WHITE = "white";
+export const BG_COLOR = BLACK;
 export const GREY_COLOR = "#bdc3c7";
 export const INACTIVE_COLOR = "#7f8c8d";
 export const ACTIVE_COLOR = WHITE;
